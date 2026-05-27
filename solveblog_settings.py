b9a68bc8d57f27359c0f457251214ffea27c3842
Rens <rd@answer.ai>
2026-05-27 15:12:00 +0200

    init

---
+++ solveblog_settings.py
@@ -0,0 +1,11 @@
+from fastcore.utils import AttrDict
+
+cfg = AttrDict(
+    name='Your Name',
+    port=8000,
+    email='',
+    description='',
+    github='',
+    twitter='',
+    linkedin='',
+)
